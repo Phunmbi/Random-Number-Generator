d83b667e8d8efea1ebad600e2f51e7f0121e7a4b
phunmbi <phunmbi@gmail.com>
2019-07-15 16:49:37 +0100

    chore(setup): configure tests and local reporting
    - setup yarn
    - configure tests with enzyme and snapshot testing
    - demonstrate snapshot testing
    
    [Finishes #167224572]

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should take screenshot successfully', () => {
+  const wrapped = mount(
+    <App />
+  );
+  expect(wrapped).toMatchSnapshot();
 });

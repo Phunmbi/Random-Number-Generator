af8ecab56b400789d13f760fd5e2eed39105e1f2
Adeniyi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2020-12-09 16:25:49 +0100

    test

--- src/Views/IndexView/index.jsx
+++ src/Views/IndexView/index.jsx
@@ -1,42 +1,136 @@
-import React, {Component} from 'react';
-import './IndexView.scss';
+import React, { useEffect, useState } from "react";
+import "./IndexView.scss";
+import DolaBuyNow from "dola-buy-now";
+import { items } from "./items";
 
-class IndexView extends Component {
-  state = {
-    amountToGenerate: 1000
-  };
+const initialState = {
+	cart: {
+		items: [],
+		total: 0,
+		weight: 0,
+	},
+};
 
-  handleChange = (e) => {
-    this.setState({
-      amountToGenerate: e.target.value
-    })
-  };
+const IndexView = () => {
+	const [cart, setCart] = useState(initialState.cart);
+	const [Dola, setDola] = useState();
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.history.push(`/generated?${this.state.amountToGenerate}`);
-  };
+	useEffect(() => {
+		const createDolaInstance = async () => {
+			const dolaInstance = await DolaBuyNow.initialize(
+				"dola_pay_107e0b90-3658-11eb-bbce-9717270f4463"
+			);
 
-  render() {
-    return (
-      <div className="generator">
-        <div className="generator-title">
-          <h2>Random Number Generator</h2>
-        </div>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <input
-            type="text"
-            value={this.state.amountToGenerate}
-            onChange={(e) => this.handleChange(e)}
-            className="generator-input"
-            placeholder="How many numbers would you like to generate?"/>
-          <div className="generator-actions">
-            <input type="submit" value="Generate Numbers"/>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+			if (dolaInstance) {
+				setDola(dolaInstance);
+			}
+		};
+
+		createDolaInstance();
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newCart = {
+			totalPrice: cart.total,
+			totalWeight: cart.weight,
+			currency: "USD",
+			discount: 0,
+			items: cart.items,
+		};
+
+		console.log(newCart, cart);
+		Dola.attachDolaToCart(newCart);
+	};
+
+	const handleClick = (e, each) => {
+		e.preventDefault();
+
+		setCart({
+			items: [...cart.items, each],
+			total: cart.total + each.price,
+			weight: cart.weight + each.grams,
+		});
+	};
+
+	const handleBuyNow = (e, item) => {
+		e.preventDefault();
+
+		setCart({
+			items: [...cart.items, item],
+			total: cart.total + item.price,
+			weight: cart.weight + item.grams,
+		});
+
+		Dola.attachDolaToItem(item, 0, "USD");
+	};
+
+	return (
+		<div className="generator">
+			<div className="generator-title">
+				<h2>Random Store</h2>
+			</div>
+			<div className="generator-title">
+				<h2>Cart: {cart.items.length}</h2>
+			</div>
+			<div className="generator-cart">
+				{items.map((each) => {
+					return (
+						<div key={each.id}>
+							<p style={{ fontWeight: "bold" }}>name: {each.title}</p>
+							<img
+								style={{ width: "300px", height: "300px" }}
+								src={each.image}
+								alt={each.title}
+							/>
+							<p>quantity: {each.quantity} </p>
+							<p>price: ${each.price.toString().slice(0, -2)} </p>
+							<p>weight: {each.grams}grams </p>
+							<div
+								style={{
+									fontWeight: "600",
+									border: "1px solid black",
+									textAlign: "center",
+									padding: "7px",
+									borderRadius: "6px",
+									cursor: "pointer",
+								}}
+								onClick={(e) => {
+									handleBuyNow(e, each);
+								}}
+							>
+								Buy now
+							</div>
+							<div
+								style={{
+									marginTop: "10px",
+									fontWeight: "600",
+									border: "1px solid black",
+									textAlign: "center",
+									padding: "7px",
+									borderRadius: "6px",
+									cursor: "pointer",
+								}}
+								onClick={(e) => {
+									handleClick(e, each);
+								}}
+							>
+								Add to Cart
+							</div>
+						</div>
+					);
+				})}
+			</div>
+			<p style={{ fontWeight: "bold", marginTop: "30px" }}>
+				Cart Total: ${cart?.total?.toString().slice(0, -2)}
+			</p>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<div className="generator-actions">
+					<input type="submit" value="Checkout with Dola" />
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default IndexView;
---
+++ src/Views/IndexView/items.js
@@ -0,0 +1,36 @@
+export const items = [
+	{
+		id: "1111",
+		image:
+			"https://target.scene7.com/is/image/Target/GUEST_448cd375-9756-4fdc-99e3-96396f30dc29?wid=488&hei=488&fmt=pjpeg",
+		quantity: 1,
+		title: "Sony Playstation 5",
+		price: 35000,
+		grams: 3000,
+		sku: "Sony Playstation 5 white",
+		variantInfo: [
+			{
+				id: "4444",
+				name: "colour",
+				value: "white",
+			},
+		],
+	},
+	{
+		id: "1131",
+		image:
+			"https://news.xbox.com/en-us/wp-content/uploads/sites/2/2019/12/XboxSeriesXHERO.jpg?fit=1920%2C1080",
+		quantity: 1,
+		title: "Xbox Series X",
+		price: 33000,
+		grams: 3000,
+		sku: "Xbox Series X black",
+		variantInfo: [
+			{
+				id: "444334",
+				name: "colour",
+				value: "black",
+			},
+		],
+	},
+];
